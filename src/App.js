7e73587bc02f8ddc929d372c5dd01cd2acb3fbf5
raviharyo <raffiiphone99@gmail.com>
2021-09-14 13:28:51 +0700

    FE test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'
+import qs from 'querystring'
+// import {Button, table} from 'bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      data: [],
+      detailsData:{
+         id_data: "",
+         nama_siswa:"",
+        waktu_data:"",
+        nilai_akhir: ""
+
+      }
+
+     }
+  }
+
+getdata = (datanilai) =>{
+  const API_URL = 'http://localhost:5003/'
+axios
+  .get(`${API_URL}`)
+  .then(res => {
+    this.setState({
+      detailsData: res.data
+    })
+    console.log(detailsData)
+  } )
+  .catch(err => console.error(err));
 }
 
-export default App;
+componentDidMount(){
+  this.getdata()
+}
+  render() { 
+    
+    return ( 
+      <>
+  <table>
+  
+     <thead>
+       <tr>
+         <th>ID</th>
+              <th>Nama</th>
+              <th>Waktu</th>
+              <th>Nilai</th>
+       </tr>
+     </thead>
+     <tbody>
+       {this.state.detailsData.map(
+        
+         detailsData=>
+         <div>
+       <tr key={detailsData.id_data}></tr>
+       <td>{detailsData.nama_siswa}</td>
+       <td>{detailsData.waktu_data}</td>
+       <td>{detailsData.nilai_akhir}</td>
+           </div>
+       )}
+     </tbody>
+          
+  </table>
+      </>
+     );
+  }
+}
+ 
+export default App ;
